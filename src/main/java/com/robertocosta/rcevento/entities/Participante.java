b7ea62ac45c6d48cac94e3cc2553ecb71a614b20
Roberto Costa <robertocosta66@gmail.com>
2023-11-28 16:00:56 -0300

    Entity Participante

---
+++ src/main/java/com/robertocosta/rcevento/entities/Participante.java
@@ -0,0 +1,60 @@
+package com.robertocosta.rcevento.entities;
+
+import java.util.Objects;
+
+public class Participante {
+
+	private Integer id;
+	private String nome;
+	private String email;
+	
+	public Participante() {
+	}
+
+	public Participante(Integer id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Participante other = (Participante) obj;
+		return Objects.equals(id, other.id);
+	}
+}
