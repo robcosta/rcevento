ca73631688e6c12c99bcd827bd2f5a0889261d54
Roberto Costa <robertocosta66@gmail.com>
2023-11-28 15:46:00 -0300

    Created Project

---
+++ src/main/java/com/robertocosta/rcevento/RceventoApplication.java
@@ -0,0 +1,13 @@
+package com.robertocosta.rcevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RceventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RceventoApplication.class, args);
+	}
+
+}
