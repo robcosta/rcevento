6b33f70d8c53056c75d900a939a4470cbc3d9b6b
Roberto Costa <robertocosta66@gmail.com>
2023-11-28 15:56:06 -0300

    Entity Categoria

---
+++ src/main/java/com/robertocosta/rcevento/entities/Categoria.java
@@ -0,0 +1,50 @@
+package com.robertocosta.rcevento.entities;
+
+import java.util.Objects;
+
+public class Categoria {
+	
+	private Integer id;
+	private String descricao;
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id);
+	}	
+}
