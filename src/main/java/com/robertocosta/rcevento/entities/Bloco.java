b119d69d6f16827d3941fe73d9d1612d1d7f8082
Roberto Costa <robertocosta66@gmail.com>
2023-11-28 15:51:17 -0300

    Entity Bloco

---
+++ src/main/java/com/robertocosta/rcevento/entities/Bloco.java
@@ -0,0 +1,61 @@
+package com.robertocosta.rcevento.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Bloco {
+	
+	private Integer id;
+	private Instant inicio;
+	private Instant fim;
+	
+	public Bloco() {
+	}
+
+	public Bloco(Integer id, Instant inicio, Instant fim) {
+		this.id = id;
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Instant inicio) {
+		this.inicio = inicio;
+	}
+
+	public Instant getFim() {
+		return fim;
+	}
+
+	public void setFim(Instant fim) {
+		this.fim = fim;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(fim, id, inicio);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bloco other = (Bloco) obj;
+		return Objects.equals(fim, other.fim) && Objects.equals(id, other.id) && Objects.equals(inicio, other.inicio);
+	}
+}
