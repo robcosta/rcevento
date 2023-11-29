c48feb8e76c3d9bfed10341ac4d7a3a004c40ad5
Roberto Costa <robertocosta66@gmail.com>
2023-11-29 08:54:08 -0300

    Entity Participante

---
+++ src/main/java/com/robertocosta/rcevento/entities/Atividade.java
@@ -0,0 +1,81 @@
+package com.robertocosta.rcevento.entities;
+
+import java.util.Objects;
+
+public class Atividade {
+
+	private Integer id;
+	private String nome;
+	private String descricao;
+	private Double preco;
+	private String imgUrl;
+	
+	public Atividade() {
+	}
+
+	public Atividade(Integer id, String nome, String descricao, Double preco, String imgUrl) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.imgUrl = imgUrl;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Atividade other = (Atividade) obj;
+		return Objects.equals(id, other.id);
+	}
+}
